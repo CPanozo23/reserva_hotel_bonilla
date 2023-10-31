124e669fb08f99373c0f275e83d5333a2d7e02b4
VERONICA BARTOLOME <you@example.com>
2023-10-31 14:47:21 -0300

    AVANCE RESERVAS CON FORMULARIO

--- app.py
+++ app.py
@@ -7,11 +7,39 @@ app = Flask(__name__)
 #Datos para hoja NOSOTROS
 from data.data_general import personal_data
 
+@app.route("/")
+def home():
+    return render_template('/index.html')
+
 #COMO LLEGAR
 @app.route('/como_llegar')
 def como_llegar():
     return render_template('como_llegar.html')
 
+@app.route('/reservas')
+def reservar():
+    return render_template('reservas.html')
+
+@app.route('/reservas/agregar', methods=["POST"])
+def agregar_reserva():
+    reservaciones = db["reservas"]
+    nombre = request.form["nombre"]
+    fecha_ingreso = request.form["fecha_ingreso"]
+    fecha_salida = request.form["fecha_salida"]
+    habitaciones = request.form["habitaciones"]
+
+    if nombre and fecha_ingreso and fecha_salida and habitaciones:
+        reserva = Reserva(nombre, fecha_ingreso, fecha_salida, habitaciones)
+        reservaciones. insert_one(reserva.toDBCollecton())
+        response= jsonify({
+            "nombre": nombre,
+            "fecha_ingreso": fecha_ingreso,
+            "fecha_salida": fecha_salida,
+            "habitaciones":habitaciones
+        })
+        return redirect(url_for("home"))
+    else:
+        return notFound()
 
 
 @app.errorhandler(404)
