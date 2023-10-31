dc1e201d79cb8ec30dbec1124097ad8c989594ee
JHSM2023 <julianmoreno23xd@gmail.com>
2023-10-31 15:02:42 -0300

    Avance nosotros

--- app.py
+++ app.py
@@ -7,13 +7,29 @@ app = Flask(__name__)
 #Datos para hoja NOSOTROS
 from data.data_general import personal_data
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 #COMO LLEGAR
 @app.route('/como_llegar')
 def como_llegar():
     return render_template('como_llegar.html')
 
 
+@app.route('/nosotros')
+def nosotros():
+    data_array = personal_data
+    def personal_data():
+        return [
+            {"nombre": "Marge Simpson", "cargo": "Gerente general"},
+            {"nombre": "Homero J. Simpson", "cargo": "Recepcionista Senior"},
+            {"nombre": "Bartolomeo Simpson", "cargo": "Chef"},
+        ]
+    return render_template('nosotros.html', objet_array=data_array)
 
+@app.route('/login')
+def contacto():
+    return render_template('contacto.hlmn')
 @app.errorhandler(404)
 def notFound(error=None):
     message = {
