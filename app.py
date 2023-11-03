f3198e4ebcd52b5e909b151cacd385a027d824f3
Rogger Villamar <you@example.com>
2023-11-03 13:34:05 -0300

    formulario de contacto funcional

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, Response, jsonify, redirect, url_for
 import database as dbase
 from models.reserva import Reserva
+from models.mensaje import Mensaje
 db = dbase.dbConnection()
 app = Flask(__name__)
 
@@ -16,22 +17,35 @@ def home():
 def como_llegar():
     return render_template('como_llegar.html')
 
-@app.route('/gestion_reservas')
-def gestion_reservas():
-    reservas_db=list (db.reservas.find())
-    return render_template('/admin/gestion_reservas.html', reservas =reservas_db)
+#CONTACTO
+@app.route("/contacto")
+def contacto():
+    return render_template('contacto.html')
 
-@app.route('/estado_reserva', methods=['POST'])
-def cambiar_estado():
-    reserva_id=request.form.get['reserva_id']
-    #db = dbase.dbConnection()
-    collection = db['reservas']
-    reserva = collection.find_one({"_id":ObjectId(reserva_id)})
-    if reserva:
-        collection.update_one({"_id": ObjectId(reserva_id)},{"$set":{"estado":"Aceptada"}})
-        return "cambiado"
-    else:
-        return "error"
+#GESTION CONTACTO
+@app.route('/gestion_contacto')
+def gestion_contacto():
+    mensajes_db = list(db.mensajes.find())
+    return render_template('admin/gestion_contacto.html', mensajes=mensajes_db)
+
+###############################################################################################################################################################################
+
+#CONTACTO: POST
+@app.route('/mensaje/agregar', methods=['POST'])
+def agregar_mensaje():
+    mensajes = db.mensaje
+    asunto = request.form['asunto']
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    correo = request.form['correo']
+    mensaje = request.form['mensaje']
+    estado = "Mensaje recibido"
+
+    mensaje = Mensaje(asunto, nombre, apellido, correo, mensaje, estado)
+    mensajes.insert_one(mensaje.to_db_collection())
+    return redirect(url_for('home'))
+
+
 
 @app.errorhandler(404)
 def notFound(error=None):
