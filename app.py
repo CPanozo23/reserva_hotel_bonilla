c4774bdf0776c3c90a7d9806f72126cd7bdbb015
Rogger Villamar <you@example.com>
2023-11-03 13:34:05 -0300

    formulario de contacto funcional

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, Response, jsonify, redirect, url_for
 import database as dbase
 from models.reserva import Reserva
+from models.mensaje import Mensaje
 db = dbase.dbConnection()
 app = Flask(__name__)
 
@@ -15,6 +16,34 @@ def home():
 @app.route('/comollegar')
 def como_llegar():
     return render_template('como_llegar.html')
+
+#CONTACTO
+@app.route("/contacto")
+def contacto():
+    return render_template('contacto.html')
+
+#GESTION CONTACTO
+@app.route('/gestion_contacto')
+def gestion_contacto():
+    mensajes_db = list(db.mensajes.find())
+    return render_template('admin/gestion_contacto.html', mensajes=mensajes_db)
+
+###############################################################################################################################################################################
+
+#CONTACTO: POST
+@app.route('/mensaje/agregar', methods=['POST'])
+def agregar_mensaje():
+    mensajes = db.mensaje
+    asunto = request.form['asunto']
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    correo = request.form['correo']
+    mensaje = request.form['mensaje']
+    estado = "Mensaje recibido"
+
+    mensaje = Mensaje(asunto, nombre, apellido, correo, mensaje, estado)
+    mensajes.insert_one(mensaje.to_db_collection())
+    return redirect(url_for('home'))
 
 
 
