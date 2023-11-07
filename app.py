2b5d0bb5bb871f1f42273f45b7a12959ca4cb38e
jheisy <you@example.com>
2023-11-07 09:03:02 -0300

    comienzo de habitaciones

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from models.reserva import Reserva
 from models.mensaje import Mensaje
 db = dbase.dbConnection()
 app = Flask(__name__)
-
+from bson import ObjectId
 #Datos para hoja NOSOTROS
 from data.data_general import personal_data
 
@@ -12,10 +12,21 @@ from data.data_general import personal_data
 def home():
     return render_template("index.html")
 
+#HABITACIONES
+@app.route('/habitaciones')
+def ver_habitaciones():
+    habitaciones_db = list(db.habitacion.find())
+    return render_template('habitaciones.html', habitaciones=habitaciones_db)
+
 #COMO LLEGAR
 @app.route('/comollegar')
 def como_llegar():
     return render_template('como_llegar.html')
+
+@app.route('/gestion_reservas')
+def gestion_reservas():
+    reservas_db=list (db.reservas.find())
+    return render_template('/admin/gestion_reservas.html', reservas =reservas_db)
 
 #CONTACTO
 @app.route("/contacto")
@@ -27,8 +38,6 @@ def contacto():
 def gestion_contacto():
     mensajes_db = list(db.mensajes.find())
     return render_template('admin/gestion_contacto.html', mensajes=mensajes_db)
-
-###############################################################################################################################################################################
 
 #CONTACTO: POST
 @app.route('/mensaje/agregar', methods=['POST'])
@@ -45,7 +54,21 @@ def agregar_mensaje():
     mensajes.insert_one(mensaje.to_db_collection())
     return redirect(url_for('home'))
 
-
+@app.route('/estado_reserva', methods=['POST'])
+def cambiar_estado():
+    reserva_id=request.form.get['reserva_id']
+    #db = dbase.dbConnection()
+    collection = db['reservas']
+    reserva = collection.find_one({"_id":ObjectId(reserva_id)})
+    if reserva:
+        collection.update_one({"_id": ObjectId(reserva_id)},{"$set":{"estado":"Aceptada"}})
+        return "cambiado"
+    else:
+        return "error"
+@app.route('/habitacion/<habitacion_id>', methods=['GET'])
+def mostrar_detalle_habitacion(habitacion_id):
+    habitacion_db= db.habitacion.find_one({'_id:objectId(habitacion_id)'})
+    return render_template('detalle_habitacion.html', habitacion=habitacion_db)
 
 @app.errorhandler(404)
 def notFound(error=None):
