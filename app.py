ac10528f0b61e2d2e7e590cabd133aee3eac8e5f
paol vasconcello <you@example.com>
2023-11-07 15:08:23 -0300

    personal

--- app.py
+++ app.py
@@ -18,14 +18,8 @@ def como_llegar():
 
 @app.route('/nosotros')
 def nosotros():
-    data_array = personal_data
-    def personal_data():
-        return [
-            {"nombre": "Marge Simpson", "cargo": "Gerente general"},
-            {"nombre": "Homero J. Simpson", "cargo": "Recepcionista Senior"},
-            {"nombre": "Bartolomeo Simpson", "cargo": "Chef"},
-        ]
-    return render_template('nosotros.html', objet_array=data_array)
+    data_array = personal_data()
+    return render_template('nosotros.html', data_array=data_array)
 
 @app.route('/login')
 def contacto():
