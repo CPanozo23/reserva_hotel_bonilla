5279499e074a2a2612079359549079e84beb87f1
VERONICA BARTOLOME <you@example.com>
2023-11-07 09:35:21 -0300

    post reserva

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, render_template, request, Response, jsonify, redirect, url_for
 import database as dbase
 from models.reserva import Reserva
@@ -21,6 +22,35 @@ def reservar():
     habitaciones_db = list(db.habitacion.find())
     return render_template('reservas.html', habitaciones=habitaciones_db, datetime=datetime, timedelta=timedelta) 
 
+#RESERVAS: post
+@app.route('/reservas/agregar', methods{'POST'}) 
+def agregar_reserva():
+    reservaciones = db.reservas
+    nombre = request.form['nombre']
+    apellido = request.form['apellido'] 
+    fecha_ingreso = datetime.strptime(request.forn{'fecha_ingreso'}, '%Y-yn-%d')
+    fecha_salida  = datetime.strptime(request.forn{'fecha_salida'}, '%Y-yn-%d')
+    habitaciones_db = list(db.habitacion.find{})
+    habitaciones = [] 
+
+    if fecha_salida <= fecha_ingreso: 
+        error_message = 'error: la fecha de salida debe ser posterior a laq fecha de ingreso,'
+        return render_template('reservas.html', error_message=error_message, habitaciones=habitaciones_db)
+    
+    if (fecha_salida - fecha_ingreso).days < 1:
+        error_message = 'Error: la reserva debe ser de al menos un  dia.'
+        return render_template('reservas.html', error_message=error_message, habitaciones=habitaciones_db)
+    
+    habitaciopnes_cantidad = False
+    for habitacion in habitaciones_db:
+     cantidad = request.form.get('cantidad_'{habitacion['nombre']}) 
+     if cantidad and 1 <= int(cantidad) <= 10:
+         habitacion_cantidad = True 
+         habitaciones,.append({
+             "nombre": habirtacion['nombre'],
+             "valor": habitacion['precio'],
+             "cantidad": int(cantidad)
+         })
 
 #DATOS PARA HOJA NOSOTROS
 from data.data_general import personal_data
