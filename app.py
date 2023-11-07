d5014075bd79517f6c407c4ac713025a8a323cf3
jheisy <you@example.com>
2023-11-07 09:03:02 -0300

    comienzo de habitaciones

--- app.py
+++ app.py
@@ -4,13 +4,19 @@ from models.reserva import Reserva
 from models.mensaje import Mensaje
 db = dbase.dbConnection()
 app = Flask(__name__)
-
+from bson import ObjectId
 #Datos para hoja NOSOTROS
 from data.data_general import personal_data
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+#HABITACIONES
+@app.route('/habitaciones')
+def ver_habitaciones():
+    habitaciones_db = list(db.habitacion.find())
+    return render_template('habitaciones.html', habitaciones=habitaciones_db)
 
 #COMO LLEGAR
 @app.route('/comollegar')
@@ -59,6 +65,10 @@ def cambiar_estado():
         return "cambiado"
     else:
         return "error"
+@app.route('/habitacion/<habitacion_id>', methods=['GET'])
+def mostrar_detalle_habitacion(habitacion_id):
+    habitacion_db= db.habitacion.find_one({'_id:objectId(habitacion_id)'})
+    return render_template('detalle_habitacion.html', habitacion=habitacion_db)
 
 @app.errorhandler(404)
 def notFound(error=None):
