6aeee4597a390efc0e355bca3deee973334f0f3b
Cecilia Panozo Díaz <122641403+CPanozo23@users.noreply.github.com>
2023-11-30 02:23:23 -0300

    Uniendo branch

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, jsonify, session, request
 
 app = Flask(__name__)
-
+app.secret_key ="dfghjkl"
 import database as dbase
 from models.reserva import Reserva
 from models.mensaje import Mensaje
@@ -10,6 +10,8 @@ from bson import ObjectId
 from datetime import datetime, timedelta 
 #Datos para hoja NOSOTROS
 from data.data_general import personal_data
+#Datos para validar usuario
+from data.data_general import personal_login
 
 #RUTAS USUARIOS NO REGISTRADOS
 @app.route("/")
@@ -59,21 +61,28 @@ def mostrar_detalle_habitacion(habitacion_id):
 #GESTION RESERVA
 @app.route('/gestion_reservas')
 def gestion_reservas():
-    reservas_db=list (db.reservas.find())
-    return render_template('/admin/gestion_reservas.html', reservas =reservas_db)
-
+    user_info = session.get('user_info')
+    if user_info:
+        reservas_db=list (db.reservas.find())
+        return render_template('/admin/gestion_reservas.html', reservas =reservas_db)
+    else:
+        return redirect(url_for('login'))
 # GESTION CONTACTO
 @app.route('/gestion_contacto')
 def gestion_contacto():
-    mensajes_db = list(db.mensajes.find())
-    return render_template('admin/gestion_contacto.html', mensajes=mensajes_db)
+    user_info = session.get('user_info')
+    if user_info:
+        mensajes_db = list(db.mensajes.find())
+        return render_template('admin/gestion_contacto.html', mensajes=mensajes_db)
+    else:
+        return redirect(url_for('login'))
 
 #INTERACCION CON BASE DE DATOS
 
 #CONTACTO: POST
 @app.route('/mensaje/agregar', methods=['POST'])
 def agregar_mensaje():
-    mensajes = db.mensaje
+    mensajes = db.mensajes
     asunto = request.form['asunto']
     nombre = request.form['nombre']
     apellido = request.form['apellido']
@@ -85,47 +94,96 @@ def agregar_mensaje():
     return redirect(url_for('home'))
 
 #RESERVAS: post
-@app.route('/reservas/agregar', methods={'POST'}) 
+@app.route('/reservas/agregar', methods=['POST'])
 def agregar_reserva():
     reservaciones = db.reservas
     nombre = request.form['nombre']
-    apellido = request.form['apellido'] 
+    apellido = request.form['apellido']
     fecha_ingreso = datetime.strptime(request.form['fecha_ingreso'], '%Y-%m-%d')
     fecha_salida = datetime.strptime(request.form['fecha_salida'], '%Y-%m-%d')
+    ciudad = request.form['ciudad']
     habitaciones_db = list(db.habitacion.find())
-    habitaciones = [] 
+    habitaciones = []
 
-    if fecha_salida <= fecha_ingreso: 
-        error_message = 'error: la fecha de salida debe ser posterior a laq fecha de ingreso,'
+    if fecha_salida <= fecha_ingreso:
+        error_message = "Error: La fecha de salida debe ser posterior a la fecha de ingreso."
         return render_template('reservas.html', error_message=error_message, habitaciones=habitaciones_db)
-    
+
     if (fecha_salida - fecha_ingreso).days < 1:
-        error_message = 'Error: la reserva debe ser de al menos un  dia.'
+        error_message = "Error: La reserva debe ser de al menos un día."
         return render_template('reservas.html', error_message=error_message, habitaciones=habitaciones_db)
-    
-    habitaciones_cantidad = False
+
+    habitacion_cantidad = False
+    total=0
     for habitacion in habitaciones_db:
         cantidad = request.form.get(f"cantidad_{habitacion['nombre']}")
         if cantidad and 1 <= int(cantidad) <= 10:
-            habitacion_cantidad = True 
+            habitacion_cantidad = True
+            total=total+(int(habitacion['precio'])*int(cantidad))
             habitaciones.append({
                 "nombre": habitacion['nombre'],
-                "valor": habitacion['precio'],
+                "valor": int(habitacion['precio']),
                 "cantidad": int(cantidad)
             })
 
-#RESERVAS: CAMBIAR ESTADO CON POST
-@app.route('/estado_reserva', methods=['POST'])
-def cambiar_estado():
-    reserva_id=request.form.get['reserva_id']
-    #db = dbase.dbConnection()
-    collection = db['reservas']
-    reserva = collection.find_one({"_id":ObjectId(reserva_id)})
-    if reserva:
-        collection.update_one({"_id": ObjectId(reserva_id)},{"$set":{"estado":"Aceptada"}})
-        return "cambiado"
-    else:
-        return "error"
+    if not habitacion_cantidad:
+        error_message = "Error: Debe ingresar al menos una cantidad de habitación válida (entre 1 y 10)."
+        return render_template('reservas.html', error_message=error_message, habitaciones=habitaciones_db)
+
+    dias = (fecha_salida - fecha_ingreso).days
+    total_reserva=total*dias
+    reserva = Reserva(nombre, apellido, fecha_ingreso, fecha_salida, ciudad, habitaciones, "Reservado", total, dias, total_reserva)
+    reservaciones.insert_one(reserva.to_db_collection())
+    return redirect(url_for('home'))
+
+#RESERVAS: CAMBIAR ESTADO -> POST
+@app.route('/estado_reserva/<reserva_id>', methods=['POST'])
+def cambiar_estado_reserva(reserva_id):
+    reservas = db.reservas
+    id_obj = ObjectId(reserva_id)
+    estado_actual = request.form['estado']
+
+    if estado_actual != "Anulado":
+        if estado_actual == "Reservado":
+            nuevo_estado = 'Confirmado'
+        elif estado_actual == "Confirmado":
+            nuevo_estado = 'Realizado'
+        elif estado_actual == "Realizado":
+            nuevo_estado = 'Anulado'
+    
+        reservas.update_one({'_id': id_obj}, {'$set': {'estado': nuevo_estado}})
+    return redirect(url_for('gestion_reservas'))
+
+#MENSAJES: CAMBIAR ESTADO -> POST
+@app.route('/estado_msj/<mensaje_id>', methods=['POST'])
+def cambiar_estado_msj(mensaje_id):
+    nuevo_estado = 'Respondido'
+    mensajes = db.mensajes
+    mensaje_id_obj = ObjectId(request.form['_id'])
+
+    mensajes.update_one({'_id': mensaje_id_obj}, {'$set': {'estado': nuevo_estado}})
+    return redirect(url_for('gestion_contacto'))
+
+#LOGIN: POST
+@app.route('/admin/login', methods=['POST'])
+def validar_usuario():
+    usuario = request.form['usuario']
+    password = request.form['password']
+    usuarios = personal_login()
+    for us in usuarios:
+        if us["user"] == usuario and us["clave"] == password:
+            # Guardar información en la sesión después del inicio de sesión
+            session['user_info'] = {"user": us["user"]}
+            return redirect(url_for('gestion_reservas'))
+    error_message = 'error: Usuario no registrado'
+    return render_template('login.html', error_message=error_message)
+
+
+#CERRAR SESIÓN
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('user_info', None)
+    return redirect(url_for('login'))
 
 @app.errorhandler(404)
 def notFound(error=None):
@@ -139,5 +197,6 @@ def notFound(error=None):
 
 #lanzar la app
 if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+    app.run(host="0.0.0.0", debug=True, port=4000)
+
     
--- models/reserva.py
+++ models/reserva.py
@@ -1,23 +1,27 @@
 class Reserva:
-    #Se complementará con más campos después de definir la clase habitación
-    def __init__(self, nombre, apellido, fecha_ingreso, fecha_salida, habitaciones, estado):
+    def __init__(self, nombre, apellido, fecha_ingreso, fecha_salida, ciudad, habitaciones, estado, total, dias, total_reserva):
         self.nombre = nombre
         self.apellido = apellido
         self.fecha_ingreso = fecha_ingreso
         self.fecha_salida = fecha_salida
+        self.ciudad = ciudad
         self.habitaciones = habitaciones
         self.estado = estado
+        self.total = total
+        self.dias = dias
+        self.total_reserva = total_reserva
 
-    def toDBCollection(self):
+    def to_db_collection(self):
         return {
             'nombre': self.nombre,
             'apellido': self.apellido,
             'fecha_ingreso': self.fecha_ingreso,
             'fecha_salida': self.fecha_salida,
+            'ciudad': self.ciudad,
             'habitaciones': self.habitaciones,
-            'estado': self.estado
+            'estado': self.estado,
+            'total': self.total,
+            'dias': self.dias,
+            'total_reserva': self.total_reserva
         }
     
-    def calcular_total(self):
-        # Agrega la lógica para calcular el total a pagar
-        pass
